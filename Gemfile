abf3da351c1201031200748439635f5d7b09312e
Matthew Fisher <matthewf@activestate.com>
2013-08-14 15:18:58 -0700

    intial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+
+gem 'sqlite3'
+
+gem 'jquery-rails'
+
+# To use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+# use Twitter Bootstrap for prettier output
+gem 'bootstrap-sass'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'better_errors'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAuthExample::Application.config.secret_token = 'feec528f10cddaa11f79e88dc97c91ead0c51732510d0f1e20436eaf284bd34242dbfe2bdc644ddb7a2b7c741be0999fb0ec4a8da85a38bf7a9b3d51b2023efa'
